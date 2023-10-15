7a17fd81fa95d99ab1aa7db21ae41561f1da4da3
CenturySturgeon <gfsolucionesintegradas@gmail.com>
2023-10-15 12:43:59 -0600

    Added TOTP to the esp8266 sketch

--- Esp8266Sketch/Esp8266Sketch.ino
+++ Esp8266Sketch/Esp8266Sketch.ino
@@ -1,6 +1,7 @@
 #include <ESP8266WiFi.h>
 #include <NTPClient.h>
 #include <WiFiUdp.h>
+#include <TOTP.h>
 
 // Replace with your network credentials
 const char* ssid = "";
@@ -9,9 +10,16 @@ const char* password = "";
 // Define NTP Server and Time Zone
 const char* ntpServerName = "pool.ntp.org";
 const int timeZone = 0; // Change this to your time zone offset in seconds
+const int daylightOffset = 0; // x hour offset for Daylight Saving Time (DST)
 
 WiFiUDP udp;
 NTPClient timeClient(udp, ntpServerName, timeZone);
+
+uint8_t hmacKey[] = {  };
+
+TOTP totp = TOTP(hmacKey, 10);
+
+String totpCode = String("");
 
 void setup() {
   // Start Serial for debugging
@@ -22,7 +30,7 @@ void setup() {
   while (WiFi.status() != WL_CONNECTED) {
     delay(1000);
     Serial.println("Connecting to WiFi...");
-  }
+  } 
   Serial.println("Connected to WiFi");
 
   // Initialize the time client
@@ -32,7 +40,15 @@ void setup() {
 
 void loop() {
   timeClient.update();
-  Serial.print("Current time: ");
-  Serial.println(timeClient.getFormattedTime());
-  delay(10000); // Print the time every 10 seconds (adjust as needed)
+
+  // Serial.print("Current time: ");
+  // Serial.println(timeClient.getFormattedTime());
+  
+  // generate the TOTP code and, if different from the previous one, print to screen
+  String newCode = String(totp.getCode(timeClient.getEpochTime()));
+  if(totpCode!= newCode) {
+    totpCode = String(newCode);
+    Serial.print("TOTP code: ");
+    Serial.println(newCode);
+  }
 }
