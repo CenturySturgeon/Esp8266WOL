212ae590a948a14eabe20dc4223c19d4b724119b
CenturySturgeon <gfsolucionesintegradas@gmail.com>
2023-10-16 09:32:40 -0600

    Fixed time synching issue on local ip by correctly setting the local gateway and adding a dns.

--- Esp8266Sketch/Esp8266Sketch.ino
+++ Esp8266Sketch/Esp8266Sketch.ino
@@ -9,7 +9,7 @@
 
 // Import the html header file
 #include "index_html.h"
-// Import the environment variables (ssid, password, static IP & hmacKey)
+// Import the environment variables (ssid, password, static IP, default local gateway (get it from your router) & hmacKey)
 #include "envVariables.h"
 
 // Define NTP Server and Time Zone (remember, most authenticator apps will use the default values, UTC0, to avoid timezone issues)
@@ -17,8 +17,8 @@ const char* ntpServerName = "pool.ntp.org";
 const int timeZone = 0; // Change this to your time zone offset in seconds
 const int daylightOffset = 0; // x hour offset for Daylight Saving Time (DST)
 
-IPAddress gateway(192, 168, 1, 1);
 IPAddress subnet(255, 255, 255, 0);
+IPAddress dns(1, 1, 1, 1);            // Cloudflare dns (can be another one like google's or a local one of your choice)
 
 // Set the webserver on port XX
 ESP8266WebServer server(80);
@@ -34,18 +34,19 @@ void setup() {
   // Start Serial for debugging
   Serial.begin(115200);
 
-  // Configures static IP address
-  if (!WiFi.config(staticIP, gateway, subnet)) {
-    Serial.println("STA Failed to configure");
-  }
-
+  // Set the Wi-Fi mode to station (the Soc connects as a client instead of becoming an access point)
+  WiFi.mode(WIFI_STA);
   // Connect to Wi-Fi
   WiFi.begin(ssid, password);
+  // Configures static IP address
+  WiFi.config(staticIP, gateway, subnet, dns);
+  // Retry connection until success
   while (WiFi.status() != WL_CONNECTED) {
     delay(1000);
     Serial.println("Connecting to WiFi...");
   } 
-  Serial.println("Connected to WiFi");
+  Serial.println("Connected to WiFi with IP: ");
+  Serial.println(WiFi.localIP());
 
   // Initialize the time client
   timeClient.begin();
@@ -58,28 +59,22 @@ void setup() {
 
   // Handle the form submission
   server.on("/submit", HTTP_POST, []() {
+    timeClient.update();
     String inputText = server.arg("inputText");
     Serial.print("Submitted Text: ");
     Serial.println(inputText);
     String newCode = String(totp.getCode(timeClient.getEpochTime()));
-    server.send(200, "text/html", "You submitted: " + inputText + " The real code: " + newCode);
+    server.send(200, "text/html", "You submitted: " + inputText + " The real code: " + newCode + " " + timeClient.getFormattedTime());
   });
 
   server.begin();
 }
 
 void loop() {
-  timeClient.update();
+  // timeClient.update();
 
   // Serial.print("Current time: ");
   // Serial.println(timeClient.getFormattedTime());
-  
-  // generate the TOTP code and, if different from the previous one, print to screen
-  // String newCode = String(totp.getCode(timeClient.getEpochTime()));
-  // if(totpCode!= newCode) {
-  //   totpCode = String(newCode);
-  //   Serial.print("TOTP code: ");
-  //   Serial.println(newCode);
-  // }
+
   server.handleClient();
 }
