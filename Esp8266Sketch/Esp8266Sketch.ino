f93f423aaf338d8970640e359bf0abc643e14e17
CenturySturgeon <gfsolucionesintegradas@gmail.com>
2023-10-19 08:26:02 -0600

    Began new authentication development

--- Esp8266Sketch/Esp8266Sketch.ino
+++ Esp8266Sketch/Esp8266Sketch.ino
@@ -6,6 +6,7 @@
 #include <ESP8266WebServerSecure.h>
 #include <ESP8266mDNS.h>
 #include <TOTP.h> // For Time-based One Time Passwords
+#include <unordered_map>
 
 // Import the html header file
 #include "index_html.h"
@@ -14,11 +15,11 @@
 
 // Define NTP Server and Time Zone (remember, most authenticator apps will use the default values, UTC0, to avoid timezone issues)
 const char* ntpServerName = "pool.ntp.org";
-const int timeZone = 0; // Change this to your time zone offset in seconds
+const int timeZone = 0;       // Change this to your time zone offset in seconds
 const int daylightOffset = 0; // x hour offset for Daylight Saving Time (DST)
 
 IPAddress subnet(255, 255, 255, 0);
-IPAddress dns(1, 1, 1, 1);            // Cloudflare DNS (can be another like google's or a local one of your choice)
+IPAddress dns(1, 1, 1, 1); // Cloudflare DNS (can be another like google's or a local one of your choice)
 
 // Set the webserver on port 80
 BearSSL::ESP8266WebServerSecure server(443);
@@ -34,7 +35,7 @@ String totpCode = String("");
 
 // getPublicIp attempts 3 times to get the router's public ip, waiting 5 seconds for each reattempt
 String getPublicIp() {
-  String ip;
+  String publicIp;
   for (int attempt = 1; attempt <= 3; attempt++) {
     WiFiClient client;
     if (client.connect("api.ipify.org", 80)) {
@@ -50,20 +51,20 @@ String getPublicIp() {
         if (millis() - timeout > 5000) {
           Serial.println("Client timeout");
           client.stop();
-          break;  // Retry on timeout
+          break; // Retry on timeout
         }
       }
 
       while (client.available()) {
-        ip = client.readStringUntil('\n');
-        Serial.println(ip);
+        publicIp = client.readStringUntil('\n');
+        Serial.println(publicIp);
       }
 
       // Close the connection
       client.stop();
 
-      if (ip.length() > 0) {
-        return ip;  // Successfully obtained the IP
+      if (publicIp.length() > 0) {
+        return publicIp; // Successfully obtained the IP
       }
 
       Serial.println("IP not received on attempt " + String(attempt));
@@ -73,55 +74,74 @@ String getPublicIp() {
 
     if (attempt < 3) {
       Serial.println("Waiting 5 seconds before reattempt...");
-      delay(5000);  // Wait 5 seconds before reattempt
+      delay(5000); // Wait 5 seconds before reattempt
     }
   }
 
-  return String();  // Return an empty string after 3 failed attempts
+  return String(); // Return an empty string after 3 failed attempts
 }
 
 // Set session username and password
 const char* http_username = "admin";
 const char* http_password = "admin";
 
-struct UserSession {
-  bool authenticated;
+// std::unordered_map<std::string, std::string> credentials = {
+//         {"admin", "admin"},
+//         {"user", "user"},
+//     };
+
+struct UserSession
+{
+  IPAddress ip;
+  bool isLoggedIn;
+  ;
 };
 
-UserSession userSessions[2];  // Adjust the array size as needed
+UserSession userSessions[2];
 
-// Simple hash function to convert an IP address to a unique integer
-int ipToSessionId(IPAddress ipAddress) {
-  uint32_t ip = ipAddress;
-  return (int)(ip % 5);  // Using modulo to limit the session IDs
-}
-
-bool is_authenticated(int sessionId) {
-  return userSessions[sessionId].authenticated;
-}
-
-void handleAuthentication(int sessionId) {
-  if (server.authenticate(http_username, http_password)) {
-    userSessions[sessionId].authenticated = true;
+bool is_authenticated(IPAddress ip)
+{
+  Serial.println("Inside is authenticated");
+  for (int i = 0; i < 2; i++)
+  {
+    Serial.println(ip.toString());
+    Serial.println(userSessions[i].ip.toString());
+    if (userSessions[i].ip == ip)
+    {
+      return true;
+    }
   }
+  return false;
 }
 
-void handleRoot() {
+void handleAuthentication(IPAddress ip)
+{
+  server.send(200, "text/plain", "not authenticated!");
+}
+
+void handleRoot()
+{
   IPAddress clientIP = server.client().remoteIP(); // Get the client's IP address
-  int sessionId = ipToSessionId(clientIP); // Use a hash of the IP as the session ID
+  Serial.println(clientIP);
+  is_authenticated(clientIP);
 
-  if (!is_authenticated(sessionId)) {
-    handleAuthentication(sessionId);
-  }
-
-  if (!is_authenticated(sessionId)) {
-    return server.requestAuthentication();
+  if (!is_authenticated(clientIP))
+  {
+    // handleAuthentication(clientIP);
+    server.send(200, "text/plain", "not authenticated!");
   }
 
   server.send(200, "text/plain", "Welcome to the protected page!");
 }
 
-void setup() {
+void setup()
+{
+  UserSession test;
+  IPAddress myip(192, 168, 1, 64);
+  test.ip = myip;
+  test.isLoggedIn = true;
+
+  userSessions[0] = test;
   // Start Serial for debugging
   Serial.begin(115200);
 
@@ -135,7 +155,7 @@ void setup() {
   while (WiFi.status() != WL_CONNECTED) {
     delay(1000);
     Serial.println("Connecting to WiFi...");
-  } 
+  }
   Serial.println("Connected to WiFi with IP: ");
   Serial.println(WiFi.localIP());
 
@@ -143,12 +163,12 @@ void setup() {
   timeClient.begin();
   timeClient.update();
 
-  if (MDNS.begin("esp8266")) {
+  if (MDNS.begin("esp8266")){
     Serial.println("MDNS responder started");
   }
 
   // Add the openssl cert and private key
-  server.getServer().setRSACert(new BearSSL::X509List(serverCert), new   BearSSL::PrivateKey(serverKey));
+  server.getServer().setRSACert(new BearSSL::X509List(serverCert), new BearSSL::PrivateKey(serverKey));
 
   // Cache SSL sessions to accelerate the TLS handshake.
   server.getServer().setCache(&serverCache);
@@ -168,7 +188,7 @@ void setup() {
     Serial.println(inputText);
     String newCode = String(totp.getCode(timeClient.getEpochTime()));
     server.send(200, "text/html", "You submitted: " + inputText + " The real code: " + newCode + " " + timeClient.getFormattedTime());
-  });
+    });
 
   server.begin();
 
